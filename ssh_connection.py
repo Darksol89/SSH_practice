eaf45d2b1c65a8a1bf4f07698b38f225f27840fa
Ivan_Chistov <Darksol89@gmail.com>
2020-04-13 15:36:36 +0300

    Обновлен модуль для коннекта по SSH

--- ssh_connection.py
+++ ssh_connection.py
@@ -11,10 +11,15 @@ ssh_client = paramiko.SSHClient()
 
 def ssh_client_connect():
     """SSH client connection to remote server"""
+    ssh_client = paramiko.SSHClient()
     ssh_client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
     ssh_client.connect(hostname=host, port=port, username=username, password=password)
+
+    return ssh_client
 
 
 def ssh_client_close():
     """SSH client close connection"""
     ssh_client.close()
+
+    return ssh_client
